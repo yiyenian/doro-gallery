8af5b983d6d6f064626b1ee1226888bc9afe2c97
jingshuai <23271055+yiyenian@users.noreply.github.com>
2025-12-03 08:40:12 +0800

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -6,7 +6,7 @@ export interface ImageProps {
   public_id: string;
   format: string;
   blurDataUrl?: string;
-  title?: string;   // 新增：标题 (例如 "案例 670")
+  title?: string;   // 标题
   prompt?: string;  // 提示词
-  url?: string;
+  url?: string;     // 完整图片链接
 }
